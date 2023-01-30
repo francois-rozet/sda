89877ed092dd62bd81b8553c880664680f0d47c8
François Rozet <francois.rozet@outlook.com>
2023-01-30 13:28:07 +0100

    🚀 Second demo (Lorenz 1996)

--- csg/nn.py
+++ csg/nn.py
@@ -2,89 +2,279 @@ r"""Neural networks and modules."""
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as fun
 
-from lampe.nn import MLP, ResMLP
 from torch import Tensor, BoolTensor, Size
 from torch.distributions import Normal
 from typing import *
+from zuko.nn import LayerNorm, MLP
 from zuko.utils import broadcast
 
 
+class FCNN(nn.Sequential):
+    r"""Creates a fully convolutional neural network (FCNN).
+
+    The architecture is inspired by ConvNeXt blocks which mix depthwise and 1 by 1
+    convolutions to improve the efficiency/accuracy trade-off.
+
+    References:
+        | A ConvNet for the 2020s (Lui et al., 2022)
+        | https://arxiv.org/abs/2201.03545
+
+    Arguments:
+        in_channels: The number of input channels.
+        out_channels: The number of output channels.
+        hidden_channels: The number of hidden channels.
+        hidden_blocks: The number of hidden blocks. Each block consists in an optional
+            normalization, a depthwise convolution, an activation and a 1 by 1
+            convolution.
+        kernel_size: The size of the convolution kernels.
+        activation: The activation function constructor. If :py:`None`, use
+            :class:`torch.nn.ReLU` instead.
+        normalize: Whether channels are normalized or not.
+        spatial: The number of spatial dimensions. Can be either 1, 2 or 3.
+        kwargs: Keyword arguments passed to :class:`torch.nn.Conv2d`.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        hidden_channels: int = 64,
+        hidden_blocks: int = 3,
+        kernel_size: int = 5,
+        activation: Callable[[], nn.Module] = None,
+        normalize: bool = False,
+        spatial: int = 2,
+        **kwargs,
+    ):
+        # Components
+        convolution = {
+            1: nn.Conv1d,
+            2: nn.Conv2d,
+            3: nn.Conv3d,
+        }.get(spatial)
+
+        if activation is None:
+            activation = nn.ReLU
+
+        if normalize:
+            normalization = lambda: LayerNorm(dim=-(spatial + 1))
+        else:
+            normalization = lambda: None
+
+        layers = [
+            convolution(
+                in_channels,
+                hidden_channels,
+                kernel_size=kernel_size,
+                padding=kernel_size // 2,
+                **kwargs,
+            )
+        ]
+
+        for i in range(hidden_blocks):
+            layers.extend([
+                normalization(),
+                convolution(
+                    hidden_channels,
+                    hidden_channels * 4,
+                    groups=hidden_channels,
+                    kernel_size=kernel_size,
+                    padding=kernel_size // 2,
+                    **kwargs,
+                ),
+                activation(),
+                convolution(hidden_channels * 4, hidden_channels, kernel_size=1),
+            ])
+
+        layers.append(
+            convolution(
+                hidden_channels,
+                out_channels,
+                kernel_size=kernel_size,
+                padding=kernel_size // 2,
+                **kwargs,
+            )
+        )
+
+        layers = filter(lambda l: l is not None, layers)
+
+        super().__init__(*layers)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.spatial = spatial
+
+    def forward(self, x: Tensor) -> Tensor:
+        dim = -(self.spatial + 1)
+        batch_shape = x.shape[:dim]
+
+        x = x.reshape(-1, *x.shape[dim:])
+        y = super().forward(x)
+        y = y.reshape(*batch_shape, *y.shape[dim:])
+
+        return y
+
+
 class TimeEmbedding(nn.Module):
-    def __init__(self, periods: int = 1):
+    r"""Creates a time embedding.
+
+    Arguments:
+        frequencies: The number of embedding frequencies.
+    """
+
+    def __init__(self, frequencies: int = 1):
         super().__init__()
 
-        self.register_buffer('periods', torch.arange(periods) + 1)
+        self.register_buffer('frequencies', 2 ** torch.arange(frequencies) * torch.pi)
 
     def forward(self, t: Tensor) -> Tensor:
-        t = self.periods * torch.pi * t[..., None]
+        t = self.frequencies * t[..., None]
         t = torch.cat((t.cos(), t.sin()), dim=-1)
 
         return t
 
 
-class ScoreNetwork(nn.Module):
+class ScoreMLP(nn.Module):
+    r"""Creates a score multi-layer perceptron (MLP).
+
+    Arguments:
+        features: The number of features.
+        frequencies: The number of embedding frequencies.
+        kwargs: The keyword arguments passed to :class:`zuko.nn.MLP`.
+    """
+
     def __init__(
         self,
         features: int,
-        periods: int = 1,
-        residual: bool = False,
+        frequencies: int = 1,
         **kwargs,
     ):
         super().__init__()
 
-        self.embedding = TimeEmbedding(periods)
+        self.embedding = TimeEmbedding(frequencies)
+        self.network = MLP(features + 2 * frequencies, features, **kwargs)
 
-        if residual:
-            self.network = ResMLP(features + 2 * periods, features, **kwargs)
-        else:
-            self.network = MLP(features + 2 * periods, features, **kwargs)
-
-    def forward(self, x: Tensor, t: Tensor) -> Tensor:
+    def forward(
+        self,
+        x: Tensor,  # (*, D)
+        t: Tensor,  # (*,)
+    ) -> Tensor:
         t = self.embedding(t)
         x, t = broadcast(x, t, ignore=1)
 
         return self.network(torch.cat((x, t), dim=-1))
 
 
-class SequenceScoreNetwork(nn.Module):
-    def __init__(self, features: int, window: int = 2, **kwargs):
+class ScoreCNN(nn.Module):
+    r"""Creates a score convolutional neural network (CNN).
+
+    Arguments:
+        channels: The number of channels (last dimension).
+        frequencies: The number of embedding frequencies.
+        kwargs: The keyword arguments passed to :class:`FCNN`.
+    """
+
+    def __init__(
+        self,
+        channels: int,
+        frequencies: int = 1,
+        **kwargs,
+    ):
         super().__init__()
 
-        self.joint = ScoreNetwork(features * window, **kwargs)
-        self.marginal = ScoreNetwork(features * (window - 1), **kwargs)
+        self.embedding = TimeEmbedding(frequencies)
+        self.network = FCNN(channels + 2 * frequencies, channels, **kwargs)
+        self.spatial = self.network.spatial
 
-        self.window = window
+    def forward(
+        self,
+        x: Tensor,  # (*, H, W, C)
+        t: Tensor,  # (*,)
+    ) -> Tensor:
+        t = self.embedding(t)
+        t = t.unflatten(-1, (1,) * self.spatial + (-1,))
 
-    def forward(self, x: Tensor, t: Tensor) -> Tensor:
-        x_j = x.unfold(-2, self.window, 1).flatten(-2)
-        s_j = self.joint(x_j, t).unflatten(-1, (-1, self.window))
-        s_j = self.fold(s_j)
+        x, t = broadcast(x, t, ignore=1)
+        x = torch.cat((x, t), dim=-1)
+        x = x.movedim(-1, -(self.spatial + 1))
+        x = self.network(x)
+        x = x.movedim(-(self.spatial + 1), -1)
 
-        x_m = x[..., 1:-1, :].unfold(-2, self.window - 1, 1).flatten(-2)
-        s_m = self.marginal(x_m, t).unflatten(-1, (-1, self.window - 1))
-        s_m = self.fold(s_m)
-        s_m = fun.pad(s_m, (0, 0, 1, 1))
+        return x
+
+
+class MarkovChainScoreNetwork(nn.Module):
+    r"""Creates a score network for a Markov chain.
+
+    Arguments:
+        features: The number of features.
+        spatial: The number of spatial dimensions.
+        order: The order of the Markov chain.
+    """
+
+    def __init__(self, features: int, spatial: int = 0, order: int = 1, **kwargs):
+        super().__init__()
+
+        if spatial > 0:
+            self.marginal = ScoreCNN(features * order, spatial=spatial, **kwargs)
+            self.joint = ScoreCNN(features * (order + 1), spatial=spatial, **kwargs)
+        else:
+            self.marginal = ScoreMLP(features * order, **kwargs)
+            self.joint = ScoreMLP(features * (order + 1), **kwargs)
+
+        self.spatial = spatial
+        self.order = order
+
+    def forward(
+        self,
+        x: Tensor,  # (*, L, D) or (*, L, H, W, C)
+        t: Tensor,  # (*,)
+    ) -> Tensor:
+        t = t.unsqueeze(-1)
+        dim = -(self.spatial + 2)
+
+        x_j = x.unfold(dim, self.order + 1, 1).flatten(-2)
+        s_j = self.joint(x_j, t).unflatten(-1, (-1, self.order + 1))
+        s_j = self.fold(s_j, dim=dim - 1)
+
+        x_m = x.narrow(dim, start=1, length=x.shape[dim] - 2)
+        x_m = x_m.unfold(dim, self.order, 1).flatten(-2)
+        s_m = self.marginal(x_m, t).unflatten(-1, (-1, self.order))
+        s_m = self.fold(s_m, dim=dim - 1)
+        s_m = self.pad(s_m, (1, 1), dim=dim)
 
         return s_j - s_m
 
     @staticmethod
-    @torch.jit.script
-    def fold(x: Tensor) -> Tensor:
-        x = x.movedim(-3, -1)
+    @torch.jit.script_if_tracing
+    def fold(x: Tensor, dim: int) -> Tensor:
+        dim = dim % x.dim()
+        x = x.movedim(dim, -1)
 
-        batch, (W, L) = x.shape[:-2], x.shape[-2:]
-        kernel = torch.eye(W).flipud().to(x).unsqueeze(0)
+        batch, (C, L) = x.shape[:-2], x.shape[-2:]
+        kernel = torch.eye(C).flipud().to(x).unsqueeze(0)
 
-        x = x.reshape(-1, W, L)
-        x = fun.conv1d(x, kernel, padding=W - 1)
+        x = x.reshape(-1, C, L)
+        x = torch.nn.functional.conv1d(x, kernel, padding=C - 1)
         x = x.reshape(batch + (-1,))
 
-        return x.transpose(-2, -1)
+        return x.movedim(-1, dim)
+
+    @staticmethod
+    @torch.jit.script_if_tracing
+    def pad(x: Tensor, padding: Tuple[int, int], dim: int) -> Tensor:
+        return torch.nn.functional.pad(x, (0, 0) * (x.dim() - dim % x.dim() - 1) + padding)
 
 
 class SubVariancePreservingSDE(nn.Module):
+    r"""Creates a sub-variance preserving SDE.
+
+    Arguments:
+        score: A score estimator.
+        shape: The shape of the state space.
+    """
+
     def __init__(self, score: nn.Module, shape: Size):
         super().__init__()
 

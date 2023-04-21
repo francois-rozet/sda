597add169aca9f131949115f60f7ef389ae7cc42
François Rozet <francois.rozet@outlook.com>
2023-04-21 13:50:26 +0200

    ⚗️ Train global score for Lorenz 1963

--- experiments/lorenz/train.py
+++ experiments/lorenz/train.py
@@ -13,7 +13,23 @@ from components.utils import *
 from utils import *
 
 
-CONFIG = {
+GLOBAL_CONFIG = {
+    # Architecture
+    'embedding': 32,
+    'hidden_channels': (32, 64),
+    'hidden_blocks': (2, 1),
+    'activation': 'SiLU',
+    # Training
+    'epochs': 1024,
+    'epoch_size': 16384,
+    'batch_size': 64,
+    'optimizer': 'AdamW',
+    'learning_rate': 1e-3,
+    'weight_decay': 1e-3,
+    'scheduler': 'linear',
+}
+
+LOCAL_CONFIG = {
     # Architecture
     'embedding': 32,
     'width': 128,
@@ -31,15 +47,66 @@ CONFIG = {
 
 
 @job(array=3, cpus=2, ram='8GB', time='06:00:00')
-def train(i: int):
-    run = wandb.init(project='ssm-lorenz', config=CONFIG)
+def train_global(i: int):
+    run = wandb.init(project='ssm-lorenz-global', config=GLOBAL_CONFIG)
     runpath = PATH / f'runs/{run.name}_{run.id}'
     runpath.mkdir(parents=True, exist_ok=True)
 
-    save_config(CONFIG, runpath)
+    save_config(GLOBAL_CONFIG, runpath)
 
     # Network
-    score = make_score(window=5, **CONFIG)
+    score = make_global_score(**GLOBAL_CONFIG)
+    sde = VPSDE(score, shape=(3, 16))
+
+    # Data
+    trainset = load_data(PATH / 'data/train.h5').unfold(1, 16, 1).flatten(0, 1)
+    validset = load_data(PATH / 'data/valid.h5').unfold(1, 16, 1).flatten(0, 1)
+
+    # Training
+    generator = loop(
+        sde,
+        trainset,
+        validset,
+        **GLOBAL_CONFIG,
+    )
+
+    for loss_train, loss_valid, lr in generator:
+        run.log({
+            'loss_train': loss_train,
+            'loss_valid': loss_valid,
+            'lr': lr,
+        })
+
+    # Save
+    torch.save(
+        score.state_dict(),
+        runpath / f'state.pth',
+    )
+
+    # Evaluation
+    chain = Lorenz63(dt=0.025)
+
+    x = sde.sample((1024,), steps=64)
+    x = x.transpose(-1, -2)
+    x = chain.postprocess(x)
+    y = chain.transition(x[:, :-1])
+
+    mse = (y - x[:, 1:]).square().mean().item()
+
+    run.log({'mse': mse})
+    run.finish()
+
+
+@job(array=3, cpus=2, ram='8GB', time='06:00:00')
+def train_local(i: int):
+    run = wandb.init(project='ssm-lorenz-local', config=LOCAL_CONFIG)
+    runpath = PATH / f'runs/{run.name}_{run.id}'
+    runpath.mkdir(parents=True, exist_ok=True)
+
+    save_config(LOCAL_CONFIG, runpath)
+
+    # Network
+    score = make_local_score(window=5, **LOCAL_CONFIG)
     sde = VPSDE(score, shape=(15,))
 
     # Data
@@ -51,7 +118,7 @@ def train(i: int):
         sde,
         trainset,
         validset,
-        **CONFIG,
+        **LOCAL_CONFIG,
     )
 
     for loss_train, loss_valid, lr in generator:
@@ -83,7 +150,8 @@ def train(i: int):
 
 if __name__ == '__main__':
     schedule(
-        train,
+        train_global,
+        train_local,
         name='Training',
         backend='slurm',
         settings={'export': 'ALL'},
--- experiments/lorenz/utils.py
+++ experiments/lorenz/utils.py
@@ -18,8 +18,25 @@ else:
 PATH.mkdir(parents=True, exist_ok=True)
 
 
-def make_score(
-    window: int,
+def make_global_score(
+    embedding: int = 32,
+    hidden_channels: Sequence[int] = (32, 64),
+    hidden_blocks: Sequence[int] = (2, 1),
+    activation: str = 'SiLU',
+    **absorb,
+) -> nn.Module:
+    return ScoreUNet(
+        channels=3,
+        embedding=embedding,
+        hidden_channels=hidden_channels,
+        hidden_blocks=hidden_blocks,
+        activation=ACTIVATIONS[activation],
+        spatial=1,
+    )
+
+
+def make_local_score(
+    window: int = 5,
     embedding: int = 32,
     width: int = 128,
     depth: int = 5,
@@ -34,12 +51,21 @@ def make_score(
     )
 
 
-def load_score(file: Path, device: str = 'cpu', **kwargs) -> nn.Module:
+def load_score(
+    file: Path,
+    local: bool = False,
+    device: str = 'cpu',
+    **kwargs,
+) -> nn.Module:
     state = torch.load(file, map_location=device)
     config = load_config(file.parent)
     config.update(kwargs)
 
-    score = make_score(**config)
+    if local:
+        score = make_local_score(**config)
+    else:
+        score = make_global_score(**config)
+
     score.load_state_dict(state)
 
     return score
